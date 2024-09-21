e7dfd73fa21a2c2b0f87aa5082713e27d94e0f58
mego <makarsamer@gmail.com>
2024-09-21 22:03:34 +0300

    created a new branch for user authentication feature.

--- app/src/main/java/com/example/ecommerceapp/EcommerceApp.kt
+++ app/src/main/java/com/example/ecommerceapp/EcommerceApp.kt
@@ -1,2 +1,10 @@
 package com.example.ecommerceapp
 
+import android.app.Application
+
+class EcommerceApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize things here, like libraries or shared preferences
+    }
+}
